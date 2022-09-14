75553f23a111173ee6d3c07c20b8c14203f43a84
Acacia Ackles <alackles@msu.edu>
2022-09-14 13:31:04 -0500

    Add in-class work

---
+++ _pages/lectures/wk1-vars.py
@@ -0,0 +1,43 @@
+# this is a comment
+
+x = 5 +4 
+variables1000000000000000 = 4
+v_a_r_i_a_b_l_e = 4
+variable = 3
+
+varname = 5
+VARNAME = 6
+
+snake_case_name = "This is a snake."
+camelCaseName = "This is a camel."
+
+print(snake_case_name)
+
+# Bad variable names
+#list = "name" - Don't do this!
+#print(list)
+#print = "hello"
+#print(print)
+
+print(varname)
+print(VARNAME)
+
+my_int = 7
+my_float = 7.1
+
+my_num = my_int + my_float
+print(my_num)
+print(7/2)
+
+apples = 20
+print("I ate " + str(apples) + " apples")
+
+#pears = int("pears")
+pears_length = len("pears")
+print(pears_length)
+
+print("Please say something: ")
+my_response = input()
+print("you said: " + my_response)
+
+print([3, 4, 5, 6])
